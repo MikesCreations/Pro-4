6a6f6381ad1e3d9844a7b2700586d4c3238131ee
Michael Roth <RothMich598@GoTVDSB.ca>
2021-03-04 01:00:32 -0500

    Add project files.

---
+++ Pro 4/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Pro_4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnDub = new System.Windows.Forms.Button();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.lblOutput2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInt
+            // 
+            this.btnInt.Location = new System.Drawing.Point(124, 214);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(95, 38);
+            this.btnInt.TabIndex = 0;
+            this.btnInt.Text = "Get integer";
+            this.btnInt.UseVisualStyleBackColor = true;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // btnDub
+            // 
+            this.btnDub.Location = new System.Drawing.Point(313, 214);
+            this.btnDub.Name = "btnDub";
+            this.btnDub.Size = new System.Drawing.Size(90, 38);
+            this.btnDub.TabIndex = 1;
+            this.btnDub.Text = "Get double";
+            this.btnDub.UseVisualStyleBackColor = true;
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(134, 80);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(63, 17);
+            this.lblMin.TabIndex = 2;
+            this.lblMin.Text = "Minimum";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(134, 142);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(66, 17);
+            this.lblMax.TabIndex = 3;
+            this.lblMax.Text = "Maximum";
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(149, 32);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(254, 17);
+            this.lblHeader.TabIndex = 4;
+            this.lblHeader.Text = "Type in a minimum and maximum value";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(260, 294);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(16, 17);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Text = "h";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(263, 77);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 22);
+            this.txtMin.TabIndex = 6;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(263, 137);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 22);
+            this.txtMax.TabIndex = 7;
+            // 
+            // lblOutput2
+            // 
+            this.lblOutput2.AutoSize = true;
+            this.lblOutput2.Location = new System.Drawing.Point(263, 315);
+            this.lblOutput2.Name = "lblOutput2";
+            this.lblOutput2.Size = new System.Drawing.Size(16, 17);
+            this.lblOutput2.TabIndex = 8;
+            this.lblOutput2.Text = "h";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput2);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblHeader);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.btnDub);
+            this.Controls.Add(this.btnInt);
+            this.Name = "MainForm";
+            this.Text = "Random math";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnDub;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label lblOutput2;
+    }
+}
+
---
+++ Pro 4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pro_4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            int Min = txtMin.Text;
+
+            
+
+
+            lblOutput.Text = Min;
+
+        }
+    }
+}
